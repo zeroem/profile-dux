efaada309379b5ecfe318307ea60ba019f1af331
Darrell Hamilton <darrell.noice@gmail.com>
2012-04-19 01:44:49 -0700

    added doc comments, normalized return values and variable naming

--- src/Dux/Profiler.php
+++ src/Dux/Profiler.php
@@ -7,6 +7,12 @@ class Profiler
     private $stack = array();
     private $profiles = array();
 
+    /**
+     * Begin a new profiler level
+     *
+     * @param string $time result of microtime.  If not passed, microtime will be called internally
+     * @return Profiler
+     */
     public function start($time=null) {
         if(!isset($time)) {
             $time = microtime();
@@ -19,6 +25,12 @@ class Profiler
         return $this;
     }
 
+    /**
+     * End the newest profiling level
+     *
+     * @param string $time result of microtime.  If not passed, microtime will be called internally
+     * @return Profiler
+     */
     public function end($time=null) {
         if(!isset($time)) {
             $time = microtime();
@@ -27,8 +39,16 @@ class Profiler
         $profile = array_pop($this->stack);
         $profile->stop = new MicroTime($time);
         array_push($this->profiles,$profile);
-    }
 
+        return $this;
+    }
+    
+    /**
+     * End any remaining profiler levels
+     *
+     * @param string $time result of microtime.  If not passed, microtime will be called internally
+     * @return Profiler
+     */
     public function halt($time = null) {
         if(!empty($this->stack)) {
             if(!isset($time)) {
@@ -39,12 +59,24 @@ class Profiler
                 $this->end($time);
             }
         }
+
+        return $this;
     }
 
-    public function addProfile($start,$stop) {
+    /**
+     * A lower level combination of start and end.
+     *
+     * Intended to allow use of the profiler to keep track of
+     * the profiling data without the overhead of multiple function calls
+     *
+     * @param string $start microtime result from the start of the profiling
+     * @param string $end microtime result from the end of the profiling
+     * @return Profiler
+     */
+    public function addProfile($start,$end) {
         $profile = new Profile();
         $profile->start = new MicroTime($start);
-        $profile->end = new MicroTime($stop);
+        $profile->end = new MicroTime($end);
 
         array_push($this->profiles, $profile);
         return $this;
