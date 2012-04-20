0db05380c88179c1dfd14f2521ab628be766a56a
Darrell Hamilton <darrell.noice@gmail.com>
2012-04-20 00:35:22 -0700

    calculate multiple aggregates at once

---
+++ src/Dux/Aggregate/GroupAggregate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Dux\Aggregate;
+
+use Dux\Profile;
+
+class GroupAggregate implements AggregateInterface
+{
+    private $aggregates = array();
+
+    public function addAggregate(AggregateInterface $agg) {
+        array_push($this->aggregates, $agg);
+        return $this;
+    }
+
+    public function processProfile(Profile $prof) {
+        foreach($this->aggregates as $agg) {
+            $agg->processProfile($prof);
+        }
+    }
+
+    public function renderAggregate() {
+        $result = "";
+        foreach($this->aggregates as $agg) {
+            $result .= $agg->renderAggregate() . "\n";
+        }
+    }
+
+    public function getAggregateValue() {
+        $result = array();
+        foreach($this->aggregates as $agg) {
+            $result[get_class($agg)] = $agg->getAggregateValue();
+        }
+    }
+}
