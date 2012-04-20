e8673d56c95ee7c780357122bafa9c657bc979d8
Darrell Hamilton <darrell.noice@gmail.com>
2012-04-20 00:40:07 -0700

    doc comments

--- src/Dux/Aggregate/AggregateInterface.php
+++ src/Dux/Aggregate/AggregateInterface.php
@@ -6,7 +6,26 @@ use Dux\Profile;
 
 interface AggregateInterface
 {
+
+    /**
+     * Incorporate the profile data into the aggregate
+     *
+     * @param Profile $prof
+     * @return AggregateInterface
+     */
     function processProfile(Profile $prof);
+
+    /**
+     * Render a string representation of the aggregate data
+     *
+     * @return string
+     */
     function renderAggregate();
+
+    /**
+     * Retrieve the numeric portion of the aggregate only
+     *
+     * @return mixed
+     */
     function getAggregateValue();
 }
