d6a0923eb95c45fb25619e0ec819806de0decb2b
Darrell Hamilton <darrell.noice@gmail.com>
2012-04-20 23:30:01 -0700

    cast duration to string so it doesn't get cast down to an int instead

--- src/Dux/Aggregate/AbstractCountingAggregate.php
+++ src/Dux/Aggregate/AbstractCountingAggregate.php
@@ -9,7 +9,10 @@ abstract class AbstractCountingAggregate implements AggregateInterface
     protected $elements = array();
 
     public function processProfile(Profile $prof) {
-        $duration = $prof->getElapsedMs();
+        // NOTE: We must make the key a string.
+        // Floats get cast to ints when used as array keys.  As such
+        // any value < 1 gets cast to 0
+        $duration = "{$prof->getElapsedMs()}";
 
         if(!isset($this->elements[$duration])) {
             $this->elements[$duration] = 0;
